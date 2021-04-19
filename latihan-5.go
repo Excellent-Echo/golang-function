48093fdb6f773a07defe608fcd1c8bbf874e1069
marwanjuna <mrwnolii@gmail.com>
2021-04-19 17:15:55 +0700

    latihan-5 on progress

---
+++ latihan-5.go
@@ -0,0 +1,14 @@
+package main
+
+func ChangeNumtoStr(number int) string {
+
+}
+
+func main() {
+	ChangeNumtoStr(100000)
+	ChangeNumtoStr(111000)
+	ChangeNumtoStr(5124000)
+	ChangeNumtoStr(1543)
+	ChangeNumtoStr(1234678)
+	ChangeNumtoStr(0)
+}
