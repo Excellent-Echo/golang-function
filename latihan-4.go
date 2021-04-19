45d5e827175500e37a8f706da31b27e5a67aa911
mohammedazez <muhamadaziz047@gmail.com>
2021-04-19 23:11:28 +0700

    latihan 4 is done

--- latihan-4.go
+++ latihan-4.go
@@ -1,1 +1,24 @@
-package helper
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var data = map[string]string{
+		"name" : "andi",
+		"umur" : "30",
+		"jarakRumah" : "50",
+		"berkeluarga" : "ya",
+	}
+
+	jarakRumah, _ := strconv.Atoi(data["jarakRumah"])
+	umur, _ := strconv.Atoi(data["umur"])
+
+
+	if jarakRumah < 100 && data["berkeluarga"] == "ya" && umur > 20 {
+		fmt.Println( data["name"] + " layak mendapat bantuan dari pemerintah")
+	}
+}
+
