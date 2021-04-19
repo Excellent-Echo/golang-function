50c2b29f407b0d9234a4bc49227cd715451907e6
mohammedazez <muhamadaziz047@gmail.com>
2021-04-19 23:35:50 +0700

    latihan 6 is done

--- latihan-6.go
+++ latihan-6.go
@@ -1,1 +1,24 @@
-package helper
+package main
+
+import "fmt"
+
+func main()  {
+	var belanja1 = map[string]int{
+		"sepatu" : 1100000,
+		"jaket" : 2200000,
+		"topi" : 594000,
+		"celana" : 803000,
+		"sweater" : 330000,
+		"kausKaki" : 110000,
+		"sabuk" : 55000,
+	}
+
+	total := 0
+	for _, value := range belanja1 {
+		//fmt.Println(value)
+		pajak := (value / 100) * 10
+		//fmt.Println(pajak)
+		total += pajak
+	}
+	fmt.Println(total)
+}
