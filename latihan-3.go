5263105a2f5fba535060e29851a4a164ab08a933
mohammedazez <muhamadaziz047@gmail.com>
2021-04-19 23:10:46 +0700

    latihan 3 is done

--- latihan-3.go
+++ latihan-3.go
@@ -1,1 +1,44 @@
-package helper
+package main
+
+import "fmt"
+
+func GenapGanjil(number ...int) {
+
+	if number == nil {
+		fmt.Println("Tidak ada angka")
+	}
+
+	var (
+		genap, ganjil int
+	)
+
+	for _, value := range number {
+		if value % 2 == 0 {
+			genap++
+			//fmt.Println("ini adalah genap", value)
+		}
+
+		if value % 2 == 1 {
+			ganjil++
+			//fmt.Println("ini adalah ganjil", value)
+		}
+	}
+	//fmt.Println(genap)
+	//fmt.Println(ganjil)
+
+	if genap > ganjil {
+		fmt.Println("Genap lebih banyak")
+	}
+
+	if ganjil > genap {
+		fmt.Println("Ganjil lebih banyak")
+	}
+}
+
+func main(){
+	GenapGanjil(1,2,3,4,5)
+	GenapGanjil(4,2)
+	GenapGanjil(10,20,30,13)
+	GenapGanjil(30,13,13,77,33,55,17,13)
+	GenapGanjil()
+}
