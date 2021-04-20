806344a2b2780ff015ffdb52db4fa23a68960f4a
ammarteuku05 <ammarteuku09@gmail.com>
2021-04-20 11:54:01 +0700

    Change Idr selesai

--- latihan/convertRupiah.go
+++ latihan/convertRupiah.go
@@ -1,5 +1,20 @@
 package latihan
 
-func CovertRupiah() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func ChangeRupiah(currency int) {
+	idr := strconv.Itoa(currency)
+	thousands := 3
+	length := len(idr)
+	if currency < 0 {
+		thousands++
+	}
+	for i := length; i > thousands; {
+		i = i - 3
+		idr = idr[:i] + "," + idr[i:]
+	}
+	fmt.Println("IDR " + idr + ",00")
 }
--- mainLatihan.go
+++ mainLatihan.go
@@ -6,10 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println("HItung Pajak")
+	fmt.Println("Hitung Pajak")
 	latihan.HitungPajak(100000)
 	fmt.Println("=================")
-
+	fmt.Println("Genap Ganjil")
 	latihan.CekGenapGanjil(1, 3, 6, 6, 2, 6)
+	fmt.Println("================")
+	fmt.Println("Change To IDR")
+	latihan.ChangeRupiah(100000)
 
 }
