b26a56f796660e91160f913b6ba99092d01da573
ardhisr <radika.kerenz1@gmail.com>
2021-04-22 10:06:56 +0700

    done task 2

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 //task 1
 func Total(total float32) {
@@ -10,7 +13,28 @@ func Total(total float32) {
 	fmt.Println(ppn, harga)
 }
 
+//task 2
+func changeToIdr(nominal int) {
+	nom := strconv.Itoa(nominal)
+	//curency := "IDR"
+	leng := len(nom)
+	var res string
+	for i := 0; i <= leng-1; i++ {
+
+		if i%3 == 0 {
+			res += string(nom[i]) + ","
+		} else {
+			res += string(nom[i])
+		}
+	}
+	fmt.Println("IDR " + res + "00")
+
+}
+
 func main() {
 	//task 1
 	Total(110000)
+
+	//task 2
+	changeToIdr(1000000)
 }
