36ff6ac86908090bba9ed4e58704422dc8d8bd98
marwanjuna <mrwnolii@gmail.com>
2021-04-23 10:39:37 +0700

    handle error latihan 1 finish

--- latihan/latihan-1.go
+++ latihan/latihan-1.go
@@ -1,7 +1,18 @@
 package latihan
 
-func Belanja(total int) (ppn int, beforePpn int) {
-	beforePpn = total * 10 / 11
-	ppn = total / 11
-	return
+import (
+	"errors"
+	"strconv"
+)
+
+func Belanja(total string) (ppn int, beforePpn int, err error) {
+	input, err := strconv.Atoi(total)
+
+	if err != nil {
+		return 0, 0, errors.New("Not a number")
+	} else {
+		beforePpn = input * 10 / 11
+		ppn = input / 11
+		return ppn, beforePpn, nil
+	}
 }
