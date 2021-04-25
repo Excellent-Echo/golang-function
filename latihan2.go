373524709ce8fa7ebd1e72ce22412ab5b63bfdfd
unknown <panduwilanthara@gmail.com>
2021-04-26 00:08:33 +0700

    revisi latihan2

--- latihan2.go
+++ latihan2.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
 )
 
 func main() {
 
-	//Input data
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("masukkan input : ")
-	scanner.Scan()
-	nominal, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	changeToIdr(1000000)
 
-	fmt.Printf("IDR %v,00", nominal)
+}
+func changeToIdr(number int) {
+
+	numstr := strconv.Itoa(number) //hasil inputan diconvert dahulu menjadi string, agar bisa ditambahkan ","
+	var tampung string             // deklarasi variabel untuk menampung hasil akhir yang telah dilooping ditambahkan ","
+
+	for i := 0; i <= len(numstr)-1; i++ {
+		// perulangan jika saat nilai i/3=0 maka ditambahkan ","
+		if i%3 == 0 {
+			tampung += string(numstr[i]) + ","
+		} else { // jika i/3 != 0 maka akan nilainya ditampung biasa tanpa ","
+			tampung += string(numstr[i])
+		}
+		fmt.Println("IDR " + tampung + "00")
+	}
 }
