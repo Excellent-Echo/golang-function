0f1d63ad85de139f200745c1600a46015ad714c9
mohammedazez <muhamadaziz047@gmail.com>
2021-04-25 23:24:05 +0700

    Handling error number 3 is done

--- helper/latihan-3.go
+++ helper/latihan-3.go
@@ -1,32 +1,49 @@
 package helper
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func validateGanjilGenap(genap int, ganjil int) (bool, error) {
+	if genap == 0 && ganjil == 0 {
+		return false, errors.New("tidak boleh kosong")
+	}
+	return true, nil
+}
+
+func catchGanjilGenap() {
+	if r := recover(); r != nil {
+		fmt.Println("keterangan error :", r)
+	} else {
+		fmt.Println("Aplikasi berjalan sempurna")
+	}
+}
 
 func GenapGanjil(number ...int) {
-	if number == nil {
-		fmt.Println("Tidak ada angka")
-	}
+	defer catchGanjilGenap()
 
 	var (
 		genap, ganjil int
 	)
-
 	for _, value := range number {
 		if value%2 == 0 {
 			genap++
-			//fmt.Println("ini adalah genap", value)
 		}
 		if value%2 == 1 {
 			ganjil++
-			//fmt.Println("ini adalah ganjil", value)
 		}
 	}
-	//fmt.Println(genap)
-	//fmt.Println(ganjil)
-	if genap > ganjil {
-		fmt.Println("angka terbanyak adalah genap")
+	if valid, err := validateGanjilGenap(genap, ganjil); valid {
+		if genap > ganjil {
+			fmt.Println("Angka terbanyak adalah genap")
+		}
+		if ganjil > genap {
+			fmt.Println("Angka terbanyak adalah ganjil")
+		}
+	} else {
+		panic(err.Error())
+		fmt.Println("err")
 	}
-	if ganjil > genap {
-		fmt.Println("angka terbanyak adalah ganjil")
-	}
+
 }
