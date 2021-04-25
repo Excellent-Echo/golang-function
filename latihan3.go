63b7d5e7cca7ec5ad3b10792dbac8c3ed751506a
unknown <panduwilanthara@gmail.com>
2021-04-26 01:05:55 +0700

    revisi latihan3 akhirnya bisa juga

--- latihan3.go
+++ latihan3.go
@@ -2,24 +2,42 @@ package main
 
 import "fmt"
 
-func main() {
-	var (
-		max, min int
-	)
+func GenapGanjil(number ...int) {
+	var num []int
+	var check string
+	var max, min int
+	num = (number)
 
-	array := [5]int{75, 60, 144, 240, 165}
-	max = array[0]
-	min = array[0]
-	for i := 1; i <= len(array)-1; i++ {
-		if array[i] > max {
-			max = array[i]
-		} else if array[i] < min {
-			min = array[i]
+	// array := [5]int{75, 60, 144, 241, 165}
+	max = num[0]
+	min = num[0]
+
+	//max min
+	if len(number) > 0 {
+		for i := 1; i < len(num); i++ {
+
+			if num[i] > max {
+				max = num[i]
+			} else if num[i] < min {
+				min = num[i]
+			}
+			if max%2 == 0 {
+				check = "angka terbanyak adalah genap"
+			} else if max%2 != 0 {
+				check = "angka terbanyak adalah ganjil"
+			}
 		}
+	} else if len(number) == 0 {
+		check = "tidak ada angka"
 	}
-	if max%2 == 0 {
-		fmt.Printf("nilai max = %v, genap", max)
-	} else {
-		fmt.Printf("nilai max = %v, ganjil", max)
-	}
+	fmt.Println(check)
+}
+
+func main() {
+
+	GenapGanjil(1, 2, 3, 4, 5)
+	GenapGanjil(4, 2)
+	GenapGanjil(10, 20, 30, 13)
+	GenapGanjil(30, 13, 13, 77, 33, 55, 17, 13)
+
 }
